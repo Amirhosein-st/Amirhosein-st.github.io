3fd62c30122f1544267dda5793f980abda1322e1
Amirhosein-st <amirhoseinsafari41@gmail.com>
2025-12-27 02:13:16 +0330

    Update vite config to support larger PWA cache size

--- vite.config.ts
+++ vite.config.ts
@@ -44,6 +44,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
         runtimeCaching: [
           {
